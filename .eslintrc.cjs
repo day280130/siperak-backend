bae5ecde0648b7d100919a749cdaf93b20a6a38c
DAY280130 <dimas.280130@gmail.com>
2023-07-15 00:09:57 +0700

    added 1 rule and removed eslint-plugin-prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,19 +4,26 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
   overrides: [],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: ['@typescript-eslint'],
   rules: {
-    'prettier/prettier': 'warn',
     indent: ['warn', 2],
     'linebreak-style': ['warn', 'unix'],
     quotes: ['warn', 'single'],
     semi: ['warn', 'always'],
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        ignoreRestSiblings: true,
+        destructuredArrayIgnorePattern: '^_.*',
+        caughtErrors: 'none',
+      },
+    ],
   },
 };
