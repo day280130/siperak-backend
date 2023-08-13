0ab6b07c7274840940771c2912f8ef71b501e9bd
DAY280130 <dimas.280130@gmail.com>
2023-08-13 14:28:45 +0700

    feat: added delete and get one product

--- src/handlers/ProductHandlers.ts
+++ src/handlers/ProductHandlers.ts
@@ -1,4 +1,6 @@
-import { ErrorResponse, SuccessResponse, serializeZodIssues } from "@src/helpers/HandlerHelpers.js";
+import { cacheDuration, makeCacheKey, queryKeys } from "@src/configs/MemcachedConfigs.js";
+import { ErrorResponse, SuccessResponse, logError, serializeZodIssues } from "@src/helpers/HandlerHelpers.js";
+import { invalidateCachedQueries, memcached } from "@src/helpers/MemcachedHelpers.js";
 import { PrismaClientKnownRequestError, prisma } from "@src/helpers/PrismaHelpers.js";
 import { productSchema } from "@src/schemas/ProductSchema.js";
 import { RequestHandler } from "express";
@@ -11,6 +13,58 @@ const getProducts: RequestHandler = async (_req, res, next) => {
       status: "success",
       message: "query success",
       datas: products,
+    } satisfies SuccessResponse);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getProduct: RequestHandler = async (req, res, next) => {
+  try {
+    const paramCode = productSchema.pick({ code: true }).safeParse(req.params);
+    if (!paramCode.success) {
+      return res.status(400).json({
+        status: "error",
+        message: "no valid product code supplied",
+      } satisfies ErrorResponse);
+    }
+
+    // const cacheKey = `product:${paramCode.data.code}`;
+    const cacheKey = makeCacheKey(queryKeys.product, paramCode.data.code);
+    try {
+      const cachedUserData = await memcached.get<string>(cacheKey);
+      // console.log("getting from cache");
+      const product = productSchema.parse(JSON.parse(cachedUserData.result));
+      memcached.touch(cacheKey, cacheDuration.short);
+      return res.status(200).json({
+        status: "success",
+        message: "product found",
+        datas: product,
+      } satisfies SuccessResponse);
+    } catch (e) {
+      /* do nothing */
+    }
+
+    const product = await prisma.product.findFirst({
+      where: { code: paramCode.data.code },
+      select: { name: true, code: true, price: true },
+    });
+
+    if (!product) {
+      return res.status(404).json({
+        status: "error",
+        message: "product with supplied code not found",
+      } satisfies ErrorResponse);
+    }
+
+    memcached
+      .set(cacheKey, JSON.stringify(product), cacheDuration.short)
+      .catch(error => logError(`${req.path} > getProduct handler`, error.reason ?? error, false));
+
+    return res.status(200).json({
+      status: "success",
+      message: "product found",
+      datas: product,
     } satisfies SuccessResponse);
   } catch (error) {
     next(error);
@@ -30,6 +84,8 @@ const createProduct: RequestHandler = async (req, res, next) => {
     const insertResult = await prisma.product.create({
       data: inputBody.data,
     });
+
+    await invalidateCachedQueries(queryKeys.product);
 
     const safeInsertResult = productSchema.parse(insertResult);
     return res.status(201).json({
@@ -75,6 +131,14 @@ const editProduct: RequestHandler = async (req, res, next) => {
       data: inputBody.data,
     });
 
+    await invalidateCachedQueries(queryKeys.product);
+
+    // const cacheKey = `product:${paramCode.data.code}`;
+    const cacheKey = makeCacheKey(queryKeys.product, paramCode.data.code);
+    memcached
+      .set(cacheKey, JSON.stringify(productSchema.parse(updateResult)), cacheDuration.short)
+      .catch(error => logError(`${req.path} > editProduct handler`, error.reason ?? error, false));
+
     const safeUpdateResult = productSchema.parse(updateResult);
     return res.status(200).json({
       status: "success",
@@ -92,8 +156,44 @@ const editProduct: RequestHandler = async (req, res, next) => {
   }
 };
 
+const deleteProduct: RequestHandler = async (req, res, next) => {
+  try {
+    const paramCode = productSchema.pick({ code: true }).safeParse(req.params);
+    if (!paramCode.success) {
+      return res.status(400).json({
+        status: "error",
+        message: "no valid product code supplied",
+      } satisfies ErrorResponse);
+    }
+
+    await prisma.product.delete({ where: { code: paramCode.data.code } });
+
+    await invalidateCachedQueries(queryKeys.product);
+
+    const cacheKey = makeCacheKey(queryKeys.product, paramCode.data.code);
+    memcached
+      .del(cacheKey)
+      .catch(error => logError(`${req.path} > deleteProduct handler`, error.reason ?? error, false));
+
+    return res.status(200).json({
+      status: "success",
+      message: "product deleted",
+    } satisfies SuccessResponse);
+  } catch (error) {
+    if (error instanceof PrismaClientKnownRequestError && error.code === "P2025") {
+      return res.status(404).json({
+        status: "error",
+        message: "product with supplied code not found",
+      } satisfies ErrorResponse);
+    }
+    next(error);
+  }
+};
+
 export const productHandlers = {
   getProducts,
+  getProduct,
   createProduct,
   editProduct,
+  deleteProduct,
 };
--- src/routers/ProductRouters.ts
+++ src/routers/ProductRouters.ts
@@ -6,5 +6,7 @@ export const productRouters = Router();
 
 const BASE_ROUTE = "/products";
 productRouters.get(`${BASE_ROUTE}`, checkAccessToken, productHandlers.getProducts);
+productRouters.get(`${BASE_ROUTE}/:code`, checkAccessToken, productHandlers.getProduct);
 productRouters.post(`${BASE_ROUTE}`, checkAccessToken, productHandlers.createProduct);
 productRouters.put(`${BASE_ROUTE}/:code`, checkAccessToken, productHandlers.editProduct);
+productRouters.delete(`${BASE_ROUTE}/:code`, checkAccessToken, productHandlers.deleteProduct);
